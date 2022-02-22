a2ee7fd1db639355393255c60090644fd5f2ccfd
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-22 10:32:18 +0100

    Finished Map component and functionality

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@mui/material';
+import MyLocationOutlineIcon from '@mui/icons-material/MyLocationOutlined'
+import Rating from '@mui/lab';
 
-import './styles.js';
+import useStyles from './styles.js';
 
 const Map = () =>  {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coordinates = { lat: 0, lng: 0}
 
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact 
+                bootstrapURLKeys={{key: process.env.REACT_GOOGLE_MAP_API}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
+    )
 }
 
 export default Map;
