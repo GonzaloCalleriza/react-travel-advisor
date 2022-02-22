57b6f2d89496e2707d26566d778b74ff712bbf81
gonzalogonzalo <calleriza.gonzalo@gmail.com>
2022-02-22 10:12:50 +0100

    Finished Header Component. Need to finetune search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@mui/material';
+import SearchIcon from '@mui/icons-material';
 
-import './styles.js';
+
+import useStyles from './styles.js';
 
 const Header = () =>  {
 
+    const classes = useStyles();
+
+    return (
+        <AppBar position='static'>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant='h5' className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Box display='flex'>
+                    <Typography variant='h6' className={classes.title}>
+                        Explore new places
+                    </Typography>
+                    <Autocomplete>
+                        <div className={classes.search}>
+                            <div className={classes.searchItem}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase plasceholder='Search...' classes={{root: classes.inputRoot, input: classes.inputInput}} />
+                        </div>
+                    </Autocomplete>
+                </Box>
+            </Toolbar>
+        </AppBar>
+    )
 }
 
 export default Header;
